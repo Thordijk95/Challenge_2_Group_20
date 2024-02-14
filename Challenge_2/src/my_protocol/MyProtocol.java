7fe45c13e90b30827221b431e5f225a54cedd5ed
Thomas Hordijk <thomasabelhordijk@gmail.com>
2024-02-14 11:35:17 +0100

    Implemented and tested with file 2, not very good scores

--- Challenge_2/src/my_protocol/MyProtocol.java
+++ Challenge_2/src/my_protocol/MyProtocol.java
@@ -3,7 +3,10 @@ package my_protocol;
 import framework.IRDTProtocol;
 import framework.Utils;
 
+import framework.Utils.Timeout;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * @version 10-07-2019
@@ -20,57 +23,93 @@ import java.util.Arrays;
  */
 public class MyProtocol extends IRDTProtocol {
 
+    static final int SWS = 6;
+
+    static final int RWS = 1;
+
     // change the following as you wish:
     static final int HEADERSIZE=1;   // number of header bytes in each packet
     static final int DATASIZE=128;   // max. number of user data bytes in each packet
 
+    List<Integer> acknowledgedSegnums = new ArrayList<>();
+    List<Integer[]> packetBuffer = new ArrayList<>();
+
     @Override
     public void sender() {
+        System.out.println("Dit is onze code");
         System.out.println("Sending...");
 
         // read from the input file
         Integer[] fileContents = Utils.getFileContents(getFileID());
-
         // keep track of where we are in the data
         int filePointer = 0;
+        int seqNum = 0;
 
-        // create a new packet of appropriate size
-        int datalen = Math.min(DATASIZE, fileContents.length - filePointer);
-        Integer[] pkt = new Integer[HEADERSIZE + datalen];
-        // write something random into the header byte
-        pkt[0] = 123;    
-        // copy databytes from the input file into data part of the packet, i.e., after the header
-        System.arraycopy(fileContents, filePointer, pkt, HEADERSIZE, datalen);
+        int LAF = -1;
+        int LFS = -1;
 
-        // send the packet to the network layer
-        getNetworkLayer().sendPacket(pkt);
-        System.out.println("Sent one packet with header="+pkt[0]);
-
-        // schedule a timer for 1000 ms into the future, just to show how that works:
-        framework.Utils.Timeout.SetTimeout(1000, this, 28);
-
-        // and loop and sleep; you may use this loop to check for incoming acks...
-        boolean stop = false;
-        while (!stop) {
-            try {
-                Thread.sleep(10);
-            } catch (InterruptedException e) {
-                stop = true;
+        while (filePointer <= fileContents.length) {
+            // create a new packet of appropriate size
+            int datalen = Math.min(DATASIZE, fileContents.length - filePointer);
+            Integer[] pkt = new Integer[HEADERSIZE + datalen];
+            // write something random into the header byte
+            if (filePointer + DATASIZE >= fileContents.length) {
+                seqNum = -1;
             }
-        }
 
+            pkt[0] = seqNum;
+            // copy databytes from the input file into data part of the packet, i.e., after the header
+            System.arraycopy(fileContents, filePointer, pkt, HEADERSIZE, datalen);
+
+            sentPacket(pkt);
+
+            LFS = seqNum;
+
+            // and loop and sleep; you may use this loop to check for incoming acks...
+            boolean stop = false;
+            while (!stop) {
+                try {
+                    Thread.sleep(10);
+                    Integer[] acknowledgePacket = getNetworkLayer().receivePacket();
+                    if (acknowledgePacket != null && (LAF < acknowledgePacket[0] || LAF == SWS)
+                        && acknowledgePacket[0] <= LFS) {
+                        acknowledgedSegnums.add(acknowledgePacket[0]);
+                        LAF = acknowledgePacket[0];
+                        System.out.println("Acknowledge received: " + acknowledgePacket[0]);
+                        stop = true;
+                    }
+                } catch (InterruptedException e) {
+                    stop = true;
+                }
+            }
+            filePointer = filePointer + DATASIZE;
+            seqNum = seqNum < SWS ? seqNum + 1 : 0;
+        }
     }
 
     @Override
     public void TimeoutElapsed(Object tag) {
         int z=(Integer)tag;
         // handle expiration of the timeout:
-        System.out.println("Timer expired with tag="+z);
+        System.out.println("this Timer expired with tag="+z);
+        // check if package was acknowledged
+        if (acknowledgedSegnums.contains((Integer)tag)) {
+            acknowledgedSegnums.remove((Integer) tag);
+        } else {
+            // Resend the packet
+            Integer[] retransmitPkt = findPacketSegNum((Integer) tag);
+            sentPacket(retransmitPkt);
+        }
     }
 
     @Override
     public Integer[] receiver() {
         System.out.println("Receiving...");
+
+        Integer[] acknowledgePacket = new Integer[1];
+
+        int LFR = -1;
+        int LAF = LFR + RWS;
 
         // create the array that will contain the file contents
         // note: we don't know yet how large the file will be, so the easiest (but not most efficient)
@@ -80,25 +119,31 @@ public class MyProtocol extends IRDTProtocol {
         // loop until we are done receiving the file
         boolean stop = false;
         while (!stop) {
-
             // try to receive a packet from the network layer
             Integer[] packet = getNetworkLayer().receivePacket();
 
             // if we indeed received a packet
             if (packet != null) {
-
                 // tell the user
                 System.out.println("Received packet, length="+packet.length+"  first byte="+packet[0] );
 
-                // append the packet's data part (excluding the header) to the fileContents array, first making it larger
-                int oldlength=fileContents.length;
-                int datalen= packet.length - HEADERSIZE;
-                fileContents = Arrays.copyOf(fileContents, oldlength+datalen);
-                System.arraycopy(packet, HEADERSIZE, fileContents, oldlength, datalen);
-
-                // and let's just hope the file is now complete
-                stop=true;
-
+                if (packet[0] <= LAF || packet[0] == 255) {
+                    acknowledgePacket[0] = packet[0];
+                    LFR = packet[0];
+                    if (LAF < SWS) {
+                        LAF = LFR + RWS;
+                    } else { LAF = 0; }
+                    // append the packet's data part (excluding the header) to the fileContents array, first making it larger
+                    getNetworkLayer().sendPacket(acknowledgePacket);
+                    System.out.println("Sent acknowledgment");
+                    int oldlength=fileContents.length;
+                    int datalen= packet.length - HEADERSIZE;
+                    fileContents = Arrays.copyOf(fileContents, oldlength+datalen);
+                    System.arraycopy(packet, HEADERSIZE, fileContents, oldlength, datalen);
+                    if (packet[0] == 255) {
+                        stop = true;
+                    }
+                }
             }else{
                 // wait ~10ms (or however long the OS makes us wait) before trying again
                 try {
@@ -108,8 +153,28 @@ public class MyProtocol extends IRDTProtocol {
                 }
             }
         }
-
         // return the output file
         return fileContents;
+    }
+
+    public void sentPacket(Integer[] pkt) {
+        // send the packet to the network layer
+        getNetworkLayer().sendPacket(pkt);
+        if (!packetBuffer.contains(pkt)) {
+            packetBuffer.add(pkt);
+        }
+        System.out.println("Sent one packet with header=" + pkt[0]);
+
+        // schedule a timer for 1000 ms into the future, just to show how that works:
+        framework.Utils.Timeout.SetTimeout(1250, this, pkt[0]);
+    }
+
+    public Integer[] findPacketSegNum(int segNum) {
+        for (int i = 0; i < packetBuffer.size(); i++) {
+            if (packetBuffer.get(i)[0] == segNum) {
+                return packetBuffer.get(i);
+            }
+        }
+        return null;
     }
 }
